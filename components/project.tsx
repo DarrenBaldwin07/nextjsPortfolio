74d83c1540e7f314db14ae0105e4564cc2208cf6
Darren B <68653294+Devd0@users.noreply.github.com>
2022-02-27 17:53:56 -0500

    project section complete - added types direct

--- components/project.tsx
+++ components/project.tsx
@@ -1,22 +1,35 @@
-import { Text, Box, HStack, Image as Img, VStack } from '@chakra-ui/react'
+import { Text, Box, HStack, Image as Img, VStack, Heading, Button, Flex, Link } from '@chakra-ui/react'
+import type { logoProps } from '../types/types'
 
 interface props {
     image: string
     heading: string
-    logos: Array<String>
+    text: string
+    logos: Array<logoProps>
+    link: string
+    github: string
 }
 // No need for return types - only do props (according to best practices) - you could do JSX.Element
-const project = ({ image, heading, logos }: props) => {
+const project = ({ image, heading, text, logos, link, github }: props) => {
   return (
     <Box w={350} boxShadow='lg' bg='brand.lightGrey' p={4} rounded='lg'>
-        <HStack>
-            <Box>
-              <VStack align='start'>
-                <Img rounded='md' src='/images/ebuy.png'/>
-                <Text fontSize={24} color='brand.lightGreen'>Ebuy</Text>
-              </VStack>
-            </Box>
-        </HStack>
+        <Box >
+          <VStack align='start' spacing={12}>
+            <VStack spacing={4} align='start'>
+              <Img rounded='md' src={image}/>
+              <Heading fontSize={24} color='brand.lightGreen'>{heading}</Heading>
+              <Text color='white'>{text}</Text>
+              <HStack>
+                <Text color='white'>Stack:</Text>
+                {logos.map(({src, alt}) => (<Img key={alt} w={6} src={src} alt={alt}/>))}
+              </HStack>
+            </VStack>
+            <HStack w='full' spacing={10}>
+              <Button variant='border' w='full'><Link _hover={{textDecoration: 'none'}} href={link}>Live Site</Link></Button>
+              <Box w={14} bg='brand.lightGreen' rounded='full'><Link href={github} isExternal><Img p={1.5} src='images/Github.svg' /></Link></Box>
+            </HStack>
+          </VStack>
+        </Box>
     </Box>
   )
 }
--- components/projectList.tsx
+++ components/projectList.tsx
@@ -1,14 +1,49 @@
-import { Box, HStack } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react"
 import Project from "./project"
 import React from 'react'
+import type { logoProps } from '../types/types'
 
+interface project {
+  image: string
+  heading: string
+  text: string
+  logos: Array<logoProps>
+  link: string
+  github: string
+}
 
 const projectList = () => {
+  const projects: Array<project> = [
+    {
+      image: 'images/ebuy.png', 
+      heading: 'Ebuy', 
+      text: 'Fullstack eccomerce CRUD application.', 
+      logos: [{src: 'images/vue.svg', alt: 'vue.js'}, {src: 'images/Tailwind.svg', alt: 'tailwind css'}, {src: 'images/supabase.svg', alt: 'supabase'}],
+      link: 'vue-crud-neon.vercel.app',
+      github: 'https://github.com/Devd0/eBuy'
+    },
+    {
+      image: 'images/ebuy.png', 
+      heading: 'Ebuy', 
+      text: 'Fullstack eccomerce CRUD application.', 
+      logos: [{src: 'images/vue.svg', alt: 'vue.js'}, {src: 'images/Tailwind.svg', alt: 'tailwind css'}, {src: 'images/supabase.svg', alt: 'supabase'}],
+      link: 'vue-crud-neon.vercel.app',
+      github: 'https://github.com/Devd0/eBuy'
+    },
+    {
+      image: 'images/ebuy.png', 
+      heading: 'Ebuy', 
+      text: 'Fullstack eccomerce CRUD application.', 
+      logos: [{src: 'images/vue.svg', alt: 'vue.js'}, {src: 'images/Tailwind.svg', alt: 'tailwind css'}, {src: 'images/supabase.svg', alt: 'supabase'}],
+      link: 'vue-crud-neon.vercel.app',
+      github: 'https://github.com/Devd0/eBuy'
+    }
+  ]
   return (
-    <Box mt={10}>
-      <HStack>
-        <Project image='' heading='' logos={['']}/>
-      </HStack>
+    <Box mt={10} mb={4}>
+      <Flex justifyContent='space-between'>
+        {projects.map(({ image, heading, text, logos, link, github}) => (<Project image={image} heading={heading} text={text} logos={logos} link={link} github={github}/>))}
+      </Flex>
     </Box>
   )
 }
