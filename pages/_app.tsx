0805f448181e41e93efdb3802fa7a23d8fc710d0
Darren B <68653294+Devd0@users.noreply.github.com>
2022-03-13 10:10:04 -0400

    portfolio complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { customTheme } from '../styles/chakra.config'
 import '@fontsource/righteous'
+import '@fontsource/roboto'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
--- types/types.ts
+++ types/types.ts
@@ -3,3 +3,12 @@ export interface logoProps {
     src: string
     alt: string
 }
+
+export interface project {
+    image: string
+    heading: string
+    text: string
+    logos: Array<logoProps>
+    link: string
+    github: string
+}
